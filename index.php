a599204162a909bc9a0327900353ac3ca3862eae
Jane Chen <jane.chen@macaulay.cuny.edu>
2017-01-07 18:19:19 -0500

    added header and footer files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'header.php';?>
+	
+	<body id="index">
+	<h1 id="main_heading">De-stress</h1>
+
+	<h2> Are you stressed? Answer these questions and see.</h2>
+	<h2>Click the button to continue</h2>
+
+	<div id="continue">
+		<a href="destress.php">
+			<button class="button">Continue</button>
+		</a>
+	</div>
+
+<?php include 'footer.php';?>
